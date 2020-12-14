16b8e3ff2eb03cc6c19457e76236f811705ce712
newhorizon-tech <alaukikinbox@gmail.com>
2020-12-14 18:43:40 +0530

    Fix rubocop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   def new
     if user_signed_in?
-     @post = current_user.posts.new
+      @post = current_user.posts.new
     else
-     flash.alert ="Please log in to create a new post"
-     redirect_to new_user_session_path
+      flash.alert = 'Please log in to create a new post'
+      redirect_to new_user_session_path
     end
   end
 
